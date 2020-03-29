6fbdfec9c5d762a6619e84bf884fcd9c92a73822
wrkbr <zcarc1@gmail.com>
2020-03-29 18:27:40 +0900

    #8 Creating a Block part Two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(
     index: number,
@@ -12,12 +22,12 @@ class Block {
     data: string,
     timestamp: number
   ) {
-      this.index = index;
-      this.hash = hash;
-      this.previousHash = previousHash;
-      this.data = data;
-      this.timestamp = timestamp;
-    }
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
 const genesisBlock: Block = new Block(0, "202020202020", "", "Hello", 123456);
