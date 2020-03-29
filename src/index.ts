9afdae56f4bc49b718ee61e1401f534208da50b9
wrkbr <zcarc1@gmail.com>
2020-03-29 18:36:17 +0900

    #9 Creating a Block part Three

--- src/index.ts
+++ src/index.ts
@@ -30,11 +30,44 @@ class Block {
   }
 }
 
+Block.calculateBlockHash
+
 const genesisBlock: Block = new Block(0, "202020202020", "", "Hello", 123456);
 
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
 
-console.log(blockChain);
+const getBlockchain = (): Block[] => blockChain;
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-export {}
+
+const createNewBlock = (data:string): Block => {
+
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimeStamp: number = getNewTimeStamp();
+
+    const newHash: string = Block.calculateBlockHash(
+        newIndex,
+        previousBlock.hash,
+        newTimeStamp,
+        data
+    );
+
+    const newBlock: Block = new Block(
+        newIndex,
+        newHash,
+        previousBlock.hash,
+        data,
+        newTimeStamp
+    );
+
+    return newBlock;
+}
+
+
+console.log(createNewBlock('hello'), createNewBlock('bye bye'));
+
+
+export {};
